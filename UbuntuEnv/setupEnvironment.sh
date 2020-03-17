7da805fbb084727522a324f4f14c136cc970cc82
Ubuntu <richardcurteis>
2020-03-17 09:07:24 -0700

    separated by folders

---
+++ UbuntuEnv/setupEnvironment.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/bash
+
+aptInstall() {
+	aptApps=(nodejs git net-tools nmap unattended-upgrades libpam-pwquality python-pip sqlmap default-jre openjdk-11-jre-headless openjdk-8-jre-headless golang-go gccgo-go ruby php7.2-cli hhvm )
+	for app in "${aptApps[@]}"
+	do
+		echo "\n[*] Installing: $app";
+		sudo /usr/bin/apt install $app -y 2>&1/dev/null 
+		installSuccess $?
+	done
+}
+
+pipInstall() {
+	pipApps=( setuptools sslyze )
+	for app in "${pipApps[@]}"
+	do
+		echo "\n[*] Installing: $1";
+		/usr/bin/pip install $1 2>&1/dev/null
+		installSuccess $?
+	done
+}
+
+installSuccess() {
+	if [ $1 -eq 0 ]; then
+    		echo "\n[~] Install Successful: $app"
+	else
+    		echo "\n[!] Install Failed: $app"
+	fi
+}
+
+main() {
+	echo "System Update and Upgrade"
+	sudo apt update && sudo apt upgrade
+	installSuccess $?
+	
+	echo "\n[*] Installing 'apt' Tools\n"
+	aptInstall()
+
+	echo "\n[*] Installing 'pip' Tools\n"
+	pipInstall()
+}
+
+main()
