0cbeabc30fc0a8bdb281c3b203ce1916996ced43
3therk1ll <richardcurteis@users.noreply.github.com>
2020-04-15 16:53:42 +0100

    Create basic_setup.sh

---
+++ UbuntuEnv/basic_setup.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/bash
+
+aptInstall() {
+	aptApps=( terminator nodejs git net-tools nmap unattended-upgrades libpam-pwquality python3 python3-pip curl default-jre openjdk-11-jre-headless openjdk-8-jre-headless hhvm )
+	for app in "${aptApps[@]}"
+	do
+		echo "[*] Installing: $app";
+		/usr/bin/sudo /usr/bin/apt install $app -y 2> /dev/null 
+		installSuccess $? $app
+	done
+}
+
+pipInstall() {
+	pipApps=( Flask )
+	for app in "${pipApps[@]}"
+	do
+		echo "[*] Installing: $app";
+		/usr/bin/pip install $app 2> /dev/null
+		installSuccess $? $app
+	done
+}
+
+installSuccess() {
+	if [ $1 -eq 0 ]; then
+    		echo 
+	else
+    		echo "[X] Install Failed: $2"
+	fi
+}
+
+main() {
+	echo "System Update and Upgrade"
+	/usr/bin/sudo apt-get update && sudo apt-get upgrade
+	installSuccess $? "System Upgrades and Updates"
+	
+	echo "[*] Installing 'apt' Tools"
+	aptInstall
+
+	echo "[*] Installing 'pip' Tools"
+	pipInstall
+}
+
+main
